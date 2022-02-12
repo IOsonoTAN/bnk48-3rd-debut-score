217513b6ac481d92dcc5bd5714ef85314912fff4
IOsonoTAN <im@tzv.me>
2022-02-13 00:00:43 +0700

    feat(core): improve performace of votes api

--- pages/api/votes.js
+++ pages/api/votes.js
@@ -1,56 +1,35 @@
 import puppeteer from 'puppeteer'
-import BigNumber from 'bignumber.js'
 import userAgent from 'user-agents'
 
-const members = [
-  "Earn",
-  "Earth",
-  "Eve",
-  "Fame",
-  "Grace",
-  "Hoop",
-  "Jaokhem",
-  "Kaofrang",
-  "Mean",
-  "Monet",
-  "Paeyah",
-  "Pampam",
-  "Pancake",
-  "Peak",
-  "Pim",
-  "Popper",
-  "Yayee",
-  "Yoghurt"
-];
-
-const getVote = (index) => {
-  return `body > div.layout-container > main > section > div.card > div.card-body > div:nth-child(11) > span > div:nth-child(${2+(3*(index+1))})`;
-}
-
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
   await page.setUserAgent(userAgent.toString())
-  await page.goto('https://scan.tokenx.finance/address/0x1c7157A8043b04258516858Ad9bD9952E0D5ec8B/read-contract');
-  await page.waitForSelector(getVote(1))
+  await page.goto('https://scan.tokenx.finance/address/0x1c7157A8043b04258516858Ad9bD9952E0D5ec8B/read-contract')
+  await page.content()
+  await page.waitForSelector('div.d-flex.py-2.border-bottom:nth-child(11) > span > div')
 
-  const ranked = []
-  const fractor = new BigNumber("1000000000000000000");
+  const rawData = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('div.d-flex.py-2.border-bottom:nth-child(11) > span > div'),
+    element => element.textContent
+  ))
+  await browser.close()
 
-  for (let index = 0; index < members.length; index++) {
-    const amount = await page.$(getVote(index))
-    let value = await page.evaluate(el => el.textContent, amount)
-    value = value.replace("(uint256) :", "");
-    value = new BigNumber(value)
-    ranked.push({
-      name: members[index],
-      vote: value.dividedBy(fractor).toFixed(10)
-    })
-  }
+  const lists = rawData.slice(3)
+  const fractor = 1000000000000000000
+
+  const members = lists.filter(list => list.includes('string')).map(name => name.split(':')[1].trim())
+  const votes = lists.filter(list => list.includes('uint256')).map(name => name.split(':')[1].trim() / fractor)
+
+  const ranked = members.map((name, index) => {
+    return {
+      name: name,
+      vote: votes[index].toFixed(10)
+    }
+  })
 
   ranked.sort((a, b) => b.vote - a.vote)
-
-  await browser.close()
 
   return res.status(200).json({
     updatedAt: new Date(),
